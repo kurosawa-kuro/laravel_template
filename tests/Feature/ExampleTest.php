a2f29ccdbcabb1ea74656c142d71a125d56fb0fe
Toshifumi Kurosawa <kuromailserver@gmail.com>
2022-12-25 11:25:49 +0900

    setup

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -7,6 +7,20 @@ use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
+    private function ddResponse($response)
+    {
+        if ($response->exception == null) {
+            $contents = json_decode(($response->baseResponse->getContent()));
+            if ($contents == null) {
+                dd(null);
+            }
+
+            dd($contents->data);
+        } else {
+            dd($response->exception);
+        }
+    }
+
     /**
      * A basic test example.
      *
@@ -15,7 +29,7 @@ class ExampleTest extends TestCase
     public function test_example()
     {
         $response = $this->get('/');
-
+        $this->ddResponse($response);
         $response->assertStatus(200);
     }
 }
